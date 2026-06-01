a1d0a162b885926261dea2533117ad3cd771ab63
Tamizha <85663828+Just-Tamizha@users.noreply.github.com>
2026-06-01 08:13:18 +0530

    Version 0.5 - Update font, Change the URL in Home Page.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-let site='http://localhost:3000';let base='/';
+let site = 'http://localhost:3000'; let base = '/';
 const deployTarget = process.env.DEPLOY_TARGET;
-if (deployTarget=="github") {
-	site='https://Just-Tamizha.github.io';
-	base='/aras-forum/';
+if (deployTarget == "github") {
+	site = 'https://Just-Tamizha.github.io';
+	base = '/aras-forum/';
 }
-else if (deployTarget=="production") {
-	site='https://aras.pingtamizha.com';
-	base='/';
+else if (deployTarget == "production") {
+	site = 'https://aras.pingtamizha.com';
+	base = '/';
 }
 
 // https://astro.build/config
 export default defineConfig({
-	site,base,
+	site, base,
 	integrations: [
 		starlight({
 			title: 'Aras Forum',
@@ -22,16 +22,18 @@ export default defineConfig({
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Just-Tamizha/aras-forum' }],
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					label: 'Get Started',
+					slug: 'get-started',
 				},
 				{
 					label: 'Reference',
 					items: [{ autogenerate: { directory: 'reference' } }],
 				},
+			],
+			customCss: [
+				// Relative path to your custom CSS file
+				'@fontsource/poppins/400.css',
+				'./src/styles/custom.css',
 			],
 		}),
 	],
---
+++ src/content/docs/get-started.md
@@ -0,0 +1,5 @@
+---
+title: Get Started !
+---
+
+Hi Everyone!
